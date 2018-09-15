087fe0ee71c249aa59ad8b4f36f243a504014b5b
paidforby <ggallo102@gmail.com>
2018-09-15 09:46:22 -0700

    test code related to #2

--- ping_example.c
+++ ping_example.c
@@ -8,12 +8,20 @@ int setup() {
   return 0;
 }
 
+int count = 0;
 
 int loop() {
-  //  Serial.printf("--- looping\n");
 
+  size_t send_len;
+  char test[256];
+  Serial.printf("--- looping\n");
+
+  send_len = snprintf(test, 256, "test packet %d \0", count);
+  send_packet(test , send_len);
+
+  count++;
   // see README.md for explanation of nsleep
-  nsleep(2, 0);
+  nsleep(5, 0);
 }
 
 
@@ -23,9 +31,9 @@ int packet_received(char* data, size_t len) {
   char foo[256];
 
   data[len] = '\0';
-  Serial.printf("received %d bytes: %s\n", len, data);
+  //Serial.printf("received %d bytes: %s\n", len, data);
 
-  send_len = snprintf(foo, 256, "Got a packet of %d bytes", (int) len);
-  send_packet(foo, send_len);
+  //send_len = snprintf(foo, 256, "Got a packet of %d bytes", (int) len);
+  //send_packet(foo, send_len);
 }
 
